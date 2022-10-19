2f55fb65f94cf82b35122b74a480074003ecad57
wann000 <wann0@yandex.ru>
2022-10-20 00:55:56 +0300

    l11

---
+++ lecii/lec_1_1_1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// text
+Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+Console.WriteLine("Privet,");
+Console.WriteLine(username);
